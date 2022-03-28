54c3b5b19d91d6156319662aea7ba2bc225f6384
Sourabh Warrier <22496373+sourabhwarrier@users.noreply.github.com>
2022-03-28 23:21:15 +0530

    dev : patch - 28-03-2022

---
+++ README.txt
@@ -0,0 +1,4 @@
+run the following commands in a linux terminal in this directory to start the application:
+
+sudo chmod 777 start.sh
+./start.sh
--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+pip install -r requirements.txt
+sudo apt install redis
 service redis-server stop
-redis-server
-
+service redis-cli stop
+coproc redis-server
+coproc (celery -A celety_async.celery_async_functions worker --loglevel=info)
+python app.py
