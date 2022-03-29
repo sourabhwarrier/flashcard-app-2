9c0f7b66b4a893e66e1ea759b5a2f355e60073f8
Sourabh Warrier <22496373+sourabhwarrier@users.noreply.github.com>
2022-03-29 22:33:32 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-pip install -r requirements.txt
-sudo apt install redis
-service redis-server stop
-service redis-cli stop
-coproc redis-server
-coproc (celery -A celety_async.celery_async_functions worker --loglevel=info)
-python app.py
