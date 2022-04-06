8ae5f0eb394c5bfddad268163caa7e40c865a481
Sourabh Warrier <22496373+sourabhwarrier@users.noreply.github.com>
2022-04-07 00:05:48 +0530

    dev : patch - 07-04-2022
    fixed celerybeat schedule issue

--- application/configuration.py
+++ application/configuration.py
@@ -1,26 +1,12 @@
 from datetime import timedelta
 from pickle import FALSE
-from celery.schedules import crontab
 import os
 class appConfig():
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_DATABASE_URI = "sqlite:///data/database.sqlite3"
     SECURITY_PASSWORD_SALT = "abcdefg"
     DEBUG = True
+    #CELERY_ENABLE_UTC = False
     CELERY_BROKER_URL='redis://localhost:6379'
     RESULT_BACKEND='redis://localhost:6379'
     #BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 31540000}
-    BEAT_SCHEDULE = {
-        'daily_revision_reminder':{
-            'task':'celery_async.reminder_async',
-            'schedule':crontab(hour=17,minute=1),
-            #'schedule':crontab(minute=1),
-            'args':()
-        },
-        'monthly_report_dispatch':{
-            'task':'celery_async.dispatch_monthly_report',
-            'schedule':crontab(day_of_month=1,hour=0,minute=0),
-            #'schedule':crontab(minute=1),
-            'args':()
-        }
-    }
--- coms/reportgen.py
+++ coms/reportgen.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML, CSS
+import os, time
 
 def generate_report(username,deckstats,date):
     css = CSS(string='''
@@ -8,7 +9,7 @@ def generate_report(username,deckstats,date):
     ''')
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('report.html')
-    html = template.render(username="user1",deckstats = deckstats,date=date)
-    with open("{}_monthly_report_{}.html".format(username,date),"w") as f:
+    html = template.render(username=username,deckstats = deckstats,date=date)
+    with open("proc/{}_monthly_report_{}.html".format(username,date),"w") as f:
         f.write(html)
-    HTML("{}_monthly_report_{}.html".format(username,date)).write_pdf("{}_monthly_report_{}.pdf".format(username,date),stylesheets=[css])
+    HTML("proc/{}_monthly_report_{}.html".format(username,date)).write_pdf("proc/{}_monthly_report_{}.pdf".format(username,date),stylesheets=[css])
